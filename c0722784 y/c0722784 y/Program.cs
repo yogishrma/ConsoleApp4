88e57eff77c09224fba0222ab9c7c6d6e72c62b2
visualstudio <visualstudio@122-PC17>
2019-02-06 10:55:54 -0500

    week 5

---
+++ c0722784 y/c0722784 y/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _730317
+{
+    class program
+    {
+        static int i = 0;
+        static void Main(string[] args)
+        {
+            MethodA();
+            Console.ReadLine();
+        }
+        public static void MethodA()
+        {
+            while (peanut())
+            {
+                Console.WriteLine("i is {0}", i);
+                if (i > 10)
+                {
+                    return;
+                }
+            }
+        }
+        public static bool peanut()
+        {
+            i++;
+            return true;
+        }
+
+
+
+
+    }
+
+}
